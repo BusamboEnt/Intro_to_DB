d6b663703deaf33f6f8bef5d2178f4b7118bd098
Arsene Busambo <79867433+BusamboEnt@users.noreply.github.com>
2025-02-20 06:50:22 +0200

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,3 @@
 INSERT INTO customer (customer_id, customer_name, email, address) 
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness Ave.");
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
